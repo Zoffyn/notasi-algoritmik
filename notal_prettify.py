98cb29bce94ae3f8c87ac8ddfa742c5dd32cd1e5
Zoffyn <ms.firdaus777@gmail.com>
2022-08-31 00:27:55 +0700

    add more style

--- notal_prettify.py
+++ notal_prettify.py
@@ -11,7 +11,6 @@ path = args.file
 tokens = []
 
 keywords = [
-    'Program',
     'integer', 'real', 'boolean', 'character',
     'true', 'false', 'and', 'or', 'xor', 'not',
     'div', 'mod', 'abs',
@@ -22,38 +21,59 @@ keywords = [
 ]
 
 special_keywords = [
-    'Program', 'procedure', 'function', 'constant'
+    'procedure', 'function', 'constant'
 ]
 
-operators = "+-*/%:<=>(),"
+headers = [
+    'Program', 'KAMUS', 'ALGORITMA'
+]
+
+operators = "+-*/%:<=>(),[]"
+
+def get_state(c: str):
+    if c.isspace():
+        return 'space'
+    elif c.isalnum():
+        return 'text'
+    elif c in operators:
+        return 'operator'
+    elif c == '{':
+        return 'comment'
+
 
 if exists(path):
     with open(path, 'r') as file:
+        state = ''
         for line in file:
-            if line[0].isspace():
-                state = 'space'
-            elif line[0].isalnum():
-                state = 'text'
-            elif line[0] in operators:
-                state = 'operator'
-            token = ''
+            if state != 'comment':
+                state = get_state(line[0])
+                token = ''
             for i in range(len(line)):
                 token += line[i]
                 if i == len(line) - 1:
-                    tokens.append(token)
+                    if state != 'comment':
+                        tokens.append(token)
                 else:
-                    if line[i+1].isspace() and (state == 'text' or state == 'operator'):
+                    if line[i+1].isspace() and state != 'space' and state != 'comment':
                         tokens.append(token)
                         token = ''
                         state = 'space'
-                    if line[i+1].isalnum() and (state == 'space' or state == 'operator'):
+                    if line[i+1].isalnum() and state != 'text' and state != 'comment':
                         tokens.append(token)
                         token = ''
                         state = 'text'
-                    if line[i+1] in operators and (state == 'space' or state == 'text'):
+                    if line[i+1] in operators and state != 'operator' and state != 'comment':
                         tokens.append(token)
                         token = ''
                         state = 'operator'
+                    if line[i+1] == '{':
+                        state = 'comment'
+                        tokens.append(token)
+                        token = ''
+                    if line[i] == '}':
+                        tokens.append(token)
+                        token = ''
+                        state = get_state(line[i+1])
 
 html = """<!DOCTYPE html>
 <html lang="en">
@@ -76,21 +96,43 @@ html = """<!DOCTYPE html>
 <body>
     <pre style='font-size: 1em; line-height: 1.15;'>"""
 
-for token in tokens:
+types = []
+procs = []
+
+for i in range(len(tokens)):
+    token = tokens[i]
     if token in keywords:
         if token in special_keywords:
-            html += f"<u><b>{token}</b></u>"
+            html += f"<u style='color: blue;'><b>{token}</b></u>"
         else:
-            html += f"<u>{token}</u>"
+            html += f"<u style='color: blue;'>{token}</u>"
+    elif token == '<-':
+        html += '&larr;'
+    elif token == '->':
+        html += '&rarr;'
+    elif token == '<=':
+        html += '&le;'
+    elif token == '>=':
+        html += '&ge;'
+    elif token[0] == '{':
+        html += f"<span style='color: green;'>{token}</span>"\
+                .replace('<-', '&larr;')\
+                .replace('->', '&rarr;')\
+                .replace('>=', '&ge;')\
+                .replace('<=', '&le;')
+    elif token in headers:
+        html += f"<b>{token}</b>"
+    elif token in types:
+        html += f"<span style='color: #27819d;'>{token}</span>"
+    elif token in procs:
+        html += f"<span style='color: #7e6128;'>{token}</span>"
+    elif i > 1 and tokens[i - 2] in ['Program', 'type']:
+        html += f"<span style='color: #27819d;'>{token}</span>"
+        types.append(token)
+    elif i > 1 and tokens[i - 2] in ['function', 'procedure']:
+        html += f"<span style='color: #7e6128;'>{token}</span>"
+        procs.append(token)
     else:
-        if token == '<-':
-            token = '&larr;'
-        if token == '->':
-            token = '&rarr;'
-        if token == '<=':
-            token = '&le;'
-        if token == '>=':
-            token = '&ge;'
         html += token
 
 html += '</pre>\n</body>\n</html>'
